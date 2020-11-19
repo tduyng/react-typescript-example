addcf61238d55f658d56653449c8012ef8009929
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-19 10:19:54 +0100

    Update build

---
+++ build/static/js/4.15d47ca6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptypescript-react-boilerplate"]=this["webpackJsonptypescript-react-boilerplate"]||[]).push([[4],{121:function(t,e,r){"use strict";r.r(e);var c=r(2);r(0);e.default=function(){return Object(c.jsx)("h1",{children:"Hi there!"})}}}]);
+//# sourceMappingURL=4.15d47ca6.chunk.js.map
